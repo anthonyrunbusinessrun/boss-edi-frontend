fc87280f913fcc43d0f4f3e058885ed15c58de13
anthonyrunbusinessrun <anthony@runbusiness.com>
2026-04-22 19:09:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['boss-edi-frontend-production.up.railway.app', 'all'],
+    host: '0.0.0.0',
+    port: 3001,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3001,
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
